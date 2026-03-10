49a37f90dd916bc65a03f7d04534731dfeb7e6a5
makar.peter98 <makar.peter98@gmail.com>
2026-03-10 15:34:54 +0100

    Ötödik óra alap, javított

--- app/src/main/java/hu/unideb/mobil/mobil_otodik_ora/MainActivity.java
+++ app/src/main/java/hu/unideb/mobil/mobil_otodik_ora/MainActivity.java
@@ -1,41 +1,36 @@
 package hu.unideb.mobil.mobil_otodik_ora;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultCaller;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     //
+    private static final String PREF_FILE = "shopping_list_pref";
+    private static final String TEXTVIEW_DATA = "TEXTVIEW_DATA";
+
     TextView itemsTextView;
     Button addButton;
     Button deleteButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        addButton = findViewById(R.id.addButton);
-        deleteButton = findViewById(R.id.deleteButton);
-        itemsTextView = findViewById(R.id.itemsTextView);
-
-        addButton.setOnClickListener( v -> activityResultLauncher.launch(new Intent(this, ItemsActivity.class)) );
-        deleteButton.setOnClickListener(v -> {itemsTextView.setText(getString(R.string.no_items));});
-    }
+    SharedPreferences sharedPreferences;
 
     ActivityResultLauncher activityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
@@ -52,4 +47,29 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
     );
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addButton = findViewById(R.id.addButton);
+        deleteButton = findViewById(R.id.deleteButton);
+        itemsTextView = findViewById(R.id.itemsTextView);
+
+        addButton.setOnClickListener( v -> activityResultLauncher.launch(new Intent(this, ItemsActivity.class)) );
+        deleteButton.setOnClickListener(v -> {itemsTextView.setText(getString(R.string.no_items));});
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
 }
